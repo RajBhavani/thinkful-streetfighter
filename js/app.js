d8293bd1155d5d2d579300dbd44864c20a461f46
Ben Boyle <benjamins.boyle@gmail.com>
2014-09-28 16:09:35 +1000

    animate hadouken fireball

--- js/app.js
+++ js/app.js
@@ -6,21 +6,36 @@ $( document ).ready(function() {
 
 
 	ryu
+	// ready position
 	.on( 'mouseenter mouseup', function() {
 		ryu
 		.addClass( 'ryu-ready' )
 		.removeClass( 'ryu-throwing ryu-still' );
-	})
-	.on( 'mouseleave', function() {
-		ryu
-		.addClass( 'ryu-still' )
-		.removeClass( 'ryu-ready ryu-throwing' );
 	})
 	// throw hadouken
 	.on( 'mousedown', function() {
 		ryu
 		.addClass( 'ryu-throwing' )
 		.removeClass( 'ryu-ready ryu-still' );
+		
+		$( '.hadouken' )
+		.show()
+		.animate(
+			{ left: '300px' },
+			500,
+			function() {
+				$( this ).hide()
+				// reset css left
+				.removeAttr( 'style' );
+			}
+		)
+
+	})
+	// restore sill pose
+	.on( 'mouseleave', function() {
+		ryu
+		.addClass( 'ryu-still' )
+		.removeClass( 'ryu-ready ryu-throwing' );
 	})
 	;
 
