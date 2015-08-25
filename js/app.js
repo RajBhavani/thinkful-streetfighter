c5c17a164034833ed6c5a96d2391dd494731a9ad
Jonas Kleiner <kleinerdesign@gmail.com>
2015-08-25 15:44:34 -0700

    Ryu responds to various interactions

--- js/app.js
+++ js/app.js
@@ -1,12 +1,26 @@
 $(document).ready(function() {
 	$('.ryu').mouseenter(function() {
-		/* this function moves Ryu to his ready position */
+		// move Ryu to his ready position */
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
 	});
 	$('.ryu').mouseleave(function() {
-		/* this function moves Ryu back to still position */
+		// move Ryu back to still position
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
+	});
+	$('.ryu').mousedown(function() {
+		// Ryu throwing position
+		// show hadouken and animate it to right of screen
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').show();
+		// play hadouken sound
+		
+	});
+	$('.ryu').mouseup(function() {
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+		// Ryu back to ready position
 	});
 });
