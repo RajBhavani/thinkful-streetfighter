ad7eed5ed6cfacd02785cfa0347121450d408286
Jonas Kleiner <kleinerdesign@gmail.com>
2015-08-25 18:12:13 -0700

    hadouken now moves across screen

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$('.ryu').mouseenter(function() {
-		// move Ryu to his ready position */
+		// move Ryu to his ready position
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
 	});
@@ -14,13 +14,49 @@ $(document).ready(function() {
 		// show hadouken and animate it to right of screen
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
+		$('.hadouken').show().animate(
+			{'left': '1020px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '520px');
+				}
+			);
 		// play hadouken sound
-		
 	});
 	$('.ryu').mouseup(function() {
+		// Ryu back to ready position
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+	});
+});
+
+/* jQuery 'chaining' method below. 'Traditional' method is above
+
+$(document).ready(function() {
+	$('.ryu').mouseenter(function() {
+		// move Ryu to his ready position
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+	})
+	.mouseleave(function() {
+		// move Ryu back to still position
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+	})
+	.mousedown(function() {
+		// Ryu throwing position
+		// show hadouken and animate it to right of screen
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').show();
+		// play hadouken sound
+	})
+	.mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 		// Ryu back to ready position
 	});
 });
+
+*/
