d5c60d9c2be86f44ca07f19f30acea21dd95c1ec
Jonas Kleiner <kleinerdesign@gmail.com>
2015-08-28 17:59:38 -0700

    Added Hadouken sound, finished hadouken anim

--- js/app.js
+++ js/app.js
@@ -10,11 +10,13 @@ $(document).ready(function() {
 		$('.ryu-still').show();
 	});
 	$('.ryu').mousedown(function() {
+		// Play hadouken sound
+		playHadouken();
 		// Ryu throwing position
 		// show hadouken and animate it to right of screen
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show().animate(
+		$('.hadouken').finish().show().animate(
 			{'left': '1020px'},
 			500,
 			function() {
@@ -22,7 +24,6 @@ $(document).ready(function() {
 				$(this).css('left', '520px');
 				}
 			);
-		// play hadouken sound
 	});
 	$('.ryu').mouseup(function() {
 		// Ryu back to ready position
@@ -30,6 +31,12 @@ $(document).ready(function() {
 		$('.ryu-ready').show();
 	});
 });
+
+function playHadouken () {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
 
 /* jQuery 'chaining' method below. 'Traditional' method is above
 
