03b431154559a9aba5cbf6e852929fb70639ecc2
Jonas Kleiner <kleinerdesign@gmail.com>
2015-08-31 17:21:28 -0700

    added cool ryu feature

--- js/app.js
+++ js/app.js
@@ -2,11 +2,13 @@ $(document).ready(function() {
 	$('.ryu').mouseenter(function() {
 		// move Ryu to his ready position
 		$('.ryu-still').hide();
+		$('.ryu-cool').hide();
 		$('.ryu-ready').show();
 	});
 	$('.ryu').mouseleave(function() {
 		// move Ryu back to still position
 		$('.ryu-ready').hide();
+		$('.ryu-cool').hide();
 		$('.ryu-still').show();
 	});
 	$('.ryu').mousedown(function() {
@@ -15,6 +17,7 @@ $(document).ready(function() {
 		// Ryu throwing position
 		// show hadouken and animate it to right of screen
 		$('.ryu-ready').hide();
+		$('.ryu-cool').hide();
 		$('.ryu-throwing').show();
 		$('.hadouken').finish().show().animate(
 			{'left': '1020px'},
@@ -28,9 +31,28 @@ $(document).ready(function() {
 	$('.ryu').mouseup(function() {
 		// Ryu back to ready position
 		$('.ryu-throwing').hide();
+		$('.ryu-cool').hide();
 		$('.ryu-ready').show();
 	});
+	$('html').keydown(function() {
+		// test if x is pressed
+		if (this.keycode=88) {
+		// show cool ryu
+			$('.ryu-still').hide();
+			$('.ryu-ready').hide();
+			$('.ryu-throwing').hide();
+			$('.ryu-cool').show();
+		};
+	});
+	$('html').keyup(function() {
+		if (true) {
+			$('.ryu-cool').hide();
+			$('.ryu-still').show();
+		};
+	});
 });
+
+
 
 function playHadouken () {
 	$('#hadouken-sound')[0].volume = 0.5;
